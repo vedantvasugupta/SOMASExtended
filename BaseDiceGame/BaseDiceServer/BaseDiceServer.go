d61b37a13321e761c9180d408690a9377ddb3188
Matthew Stevenson <matthew.stevenson21@imperial.ac.uk>
2024-11-14 09:55:19 +0000

    filling out basediceserver interface and struct fields

--- BaseDiceGame/BaseDiceServer/BaseDiceServer.go
+++ BaseDiceGame/BaseDiceServer/BaseDiceServer.go
@@ -7,20 +7,36 @@ import (
 	baseServer "github.com/MattSScott/basePlatformSOMAS/v2/pkg/server"
 //	uuid "github.com/google/uuid"
 )
-
+//based on methods defined in cw_structure_plan
+// unsure if necessary based on counter agent example
 type IBaseDiceServer interface{
 	baseServer.IServer[baseDiceAgent.IBaseDiceAgent]
-	
+	createServer(int, int, int, int, int) *IBaseDiceServer 
+	formTeams()
+	voteforArticlesofAssociation()
+	runTurn()
+	manageResources()
+	generateReport()
+	audit()
+	modifyRules()
+	verifyThreshold()
 }
 
 
 type BaseDiceServer struct{
 	*baseServer.BaseServer[baseDiceAgent.IBaseDiceAgent]
 	team common.Team
-	
-
+	turns int
+	teamSize int
+	numAgents int
+	rounds int
+	threshold int
 }
 
 
+// TEAM 2 METHODS BELOW
 
+
+
+// TEAM 5 METHODS
 
