6357dd679a69a33dc913e74be6d5e0e39864b152
dhruvds12 <dhruv.devgan@yahoo.com>
2024-11-17 22:58:13 +0000

    added baseArticlesOfAssocation

---
+++ BaseDiceGame/BaseArticlesOfAssociation/BaseArticleOfAssociation.go
@@ -0,0 +1,22 @@
+package BaseArticlesOfAssociation
+
+import "SOMASExtended/BaseDiceGame/BaseDiceAgent"
+
+
+type IArticlesOfAssociation interface {
+    // Method to calculate the expected contribution for a given agent
+    GetExpectedContribution(agent BaseDiceAgent.IBaseDiceAgent, strategy int) int
+
+    // Method to check if an agent's action complies with the AoA
+    IsAgentCompliant(agent BaseDiceAgent.IBaseDiceAgent, strategy int) bool
+
+    // Method to check how much an agent should take from the common pool
+    GetExpectedWithdrawal(agent BaseDiceAgent.IBaseDiceAgent, strategy int) int
+
+
+}
+
+func IsAgentCompliant(agent BaseDiceAgent.IBaseDiceAgent, strategy int) bool {
+    // Placeholder
+    return true
+}
