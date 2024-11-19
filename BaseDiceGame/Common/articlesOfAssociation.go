6eb62b98d925714cfb1aafa7fa1a2acf8cb7482e
Arnav Kohli <arnavkohli2003@gmail.com>
2024-11-19 23:26:18 +0000

    feat: refactored aoa code
    Signed-off-by: Arnav Kohli <arnavkohli2003@gmail.com>

--- BaseDiceGame/Common/articlesOfAssociation.go
+++ BaseDiceGame/Common/articlesOfAssociation.go
@@ -1,4 +1,4 @@
-package common
+package Common
 
 import "SOMASExtended/BaseDiceGame/BaseDiceAgent"
 
@@ -9,6 +9,9 @@ type IArticlesOfAssociation interface {
     // Method to check if an agent's action complies with the AoA
     IsAgentCompliant(agent BaseDiceAgent.IBaseDiceAgent) bool
 
+    // Method to impose penalty on agent that fails an audit - T: Agent alive, F: Agent dead
+    ImposePenalty(agent BaseDiceAgent.IBaseDiceAgent)
+
     // Method to check how much an agent should take from the common pool
     // GetExpectedWithdrawal(agent BaseDiceAgent.IBaseDiceAgent) int
 }
@@ -17,13 +20,12 @@ type IArticlesOfAssociation interface {
 type ContributionRule int
 const (
     None ContributionRule = iota
-    TenPercentAboveThreshold
     ThirtyPercentAboveThreshold
     SixtyPercentAboveThreshold
-    NinetyPercentAboveThreshold
     All
 )
 
+// Enum for costs related to an audit
 type AuditCost int
 const (
     NoCost AuditCost = iota
@@ -31,6 +33,7 @@ const (
     TwentyPercentCurrentScore
 )
 
+// Enum for how to handle an agent that fails an audit
 type AuditFailureStrategy int
 const (
     NoPenalty AuditFailureStrategy = iota
@@ -44,20 +47,34 @@ type ArticlesOfAssociation struct {
     auditFailureStrategy AuditFailureStrategy
 }
 
+// TODO: Store all previous scores of all agents on the server, and use that to see if the agents are behaving
 func (aoa *ArticlesOfAssociation) IsAgentCompliant(agent BaseDiceAgent.IBaseDiceAgent) bool {
+    // Basic implementation, but the ideas is the same
     agentScore := agent.GetScore()
     agentContribution := agent.MakeContribution()
     switch aoa.contributionRule {
-    case TenPercentAboveThreshold:
-        return agentContribution >= int(float64(agentScore) * 0.1)
     case ThirtyPercentAboveThreshold:
         return agentContribution >= int(float64(agentScore) * 0.3)
     case SixtyPercentAboveThreshold:
         return agentContribution >= int(float64(agentScore) * 0.6)
-    case NinetyPercentAboveThreshold:
-        return agentContribution >= int(float64(agentScore) * 0.9)
     case All:
         return agentContribution == agentScore
+    default:
+        return true
+    }
+}
+
+func (aoa *ArticlesOfAssociation) ImposePenalty (agent BaseDiceAgent.IBaseDiceAgent) bool {
+    switch aoa.auditFailureStrategy {
+    case TwoWarnings:
+        if agent.GetAuditViolations() == 1 {
+            return false
+        } else {
+            agent.AddAuditViolation()
+            return true
+        }
+    case ImmediateExpulsion:
+        return false
     default:
         return true
     }
